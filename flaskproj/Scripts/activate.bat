1d494883b568a872f6b784a33b6c3a6a99cd47d8
Vedang Savadi <vedang.guru@gmail.com>
2024-01-07 17:23:57 +0530

    venv updated

--- flaskproj/Scripts/activate.bat
+++ flaskproj/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Dendrite.ai Project\flaskproj
+set VIRTUAL_ENV=D:\ToDoList\flaskproj
 
 if not defined PROMPT set PROMPT=$P$G
 
